772592c867314f87031875aa6ef7e9cfd483f878
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-06-12 00:35:35 +0530

    modified

--- XyloPhone/lib/main.dart
+++ XyloPhone/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +10,35 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              }, child: Text('Click Me'),),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                }, child: Text('Click Me'),),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                }, child: Text('Click Me'),),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                }, child: Text('Click Me'),)
+            ],
+          ),
         ),
       ),
     );
