58a7b23cc5d978dc33ed229ece0201121f88212c
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-06-12 23:55:07 +0530

    modified

--- QuizzlerFlutter/lib/main.dart
+++ QuizzlerFlutter/lib/main.dart
@@ -59,10 +59,10 @@ class _QuizPageState extends State<QuizPage> {
   Question q1 = Question(q: 'You can lead a cow down stairs but not up stairs.', a: false);*/
 
   List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-  ]
+    Question(q: 'You can lead a cow down stairs but not up stairs.',a: false),
+    /*Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),*/
+  ];
   var questionNumber = 0;
   @override
   Widget build(BuildContext context) {
---
+++ QuizzlerFlutter/lib/quiz_brain.dart
@@ -0,0 +1,5 @@
+import 'Question.dart';
+
+class QuizBrain{
+
+}
