ad7494c84c811b4669183477caac766405a7a177
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-06-15 07:39:05 +0530

    modified

--- BMICalculator/lib/input_page.dart
+++ BMICalculator/lib/input_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bmi_calculator/reusable_card.dart';
+import 'icon_content.dart';
+import 'constants.dart';
 
-const bottomContainerHeight = 80.0;
-const inactiveCardColor = Color(0xF111328);
-const activeCardColor = Color(0xFF1D1E33);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -57,7 +57,7 @@ class _InputPageState extends State<InputPage> {
                    });
                    // selectedGender = Gender.male;
                  },
-                  colour: selectedGender == Gender.male? activeCardColor: inactiveCardColor,
+                  colour: selectedGender == Gender.male? kActiveCardColor: kInactiveCardColor,
                   cardChild: IconContent(
                     customIcon: Icon(FontAwesomeIcons.male, size: 80.0,),
                       customText: 'Male'
@@ -71,7 +71,7 @@ class _InputPageState extends State<InputPage> {
                     selectedGender = Gender.female;
                   });
                 },
-                colour: selectedGender == Gender.female? activeCardColor: inactiveCardColor,
+                colour: selectedGender == Gender.female? kActiveCardColor: kInactiveCardColor,
                 cardChild: IconContent(customIcon: Icon(FontAwesomeIcons.female, size: 80), customText: 'Female'),
               ))
             ],
@@ -79,13 +79,18 @@ class _InputPageState extends State<InputPage> {
           )),
           Expanded(child: Row(
             children: [
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
+              Expanded(child: ReusableCard(
+                onPress: (){
+
+                },
+                colour: kActiveCardColor,
+                cardChild: Column(
+                  children: [
+                    Text('Height',
+                    style: kLabelTextStyle,),
+                  ],
                 ),
-              ))
+              ),)
             ],
           )),
           Expanded(child: Row(
@@ -114,7 +119,7 @@ class _InputPageState extends State<InputPage> {
             color: Color(0xFFEB1555),
             margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
-            height: bottomContainerHeight,
+            height: kBottomContainerHeight,
           ) ,
         ],
       )
@@ -123,31 +128,5 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class IconContent extends StatelessWidget {
-  // const IconContent({Key? key,}) : super(key: key);
-  final Icon customIcon;
-  final String customText;
-  IconContent({required this.customIcon, required this.customText});
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        customIcon,
-        /*Icon(
-          FontAwesomeIcons.mars,
-          size: 80.0,
-        ),*/
-        SizedBox(
-          height: 20,
-        ),
-        Text(
-          customText,
-          style: TextStyle(fontSize: 18.0, color: Color(0xFF8DBE98)),
-        )
-      ],
-    );
-  }
-}
 
