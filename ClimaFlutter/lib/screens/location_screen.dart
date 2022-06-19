4114062fea532a081136d81b80bd33384237f797
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-06-19 13:55:43 +0530

    modified

--- ClimaFlutter/lib/screens/location_screen.dart
+++ ClimaFlutter/lib/screens/location_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:clima/services/weather.dart';
+import 'city_screen.dart';
 
 class LocationScreen extends StatefulWidget {
 
@@ -71,7 +72,10 @@ class _LocationScreenState extends State<LocationScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async{
+                      var weatherData = await weather.getLocationWeather();
+                      updateUI(weatherData);
+                    },
                     child: Icon(
                       Icons.near_me,
                       size: 50.0,
@@ -79,8 +83,14 @@ class _LocationScreenState extends State<LocationScreen> {
                   ),
                   FlatButton(
                     onPressed: () async{
-                      var weatherData = await weather.getLocationWeather();
-                      updateUI(weatherData);
+                    var typedName = await Navigator.push(context, MaterialPageRoute(builder: (context){
+                        return CityScreen();
+                      },),);
+                    print(typedName);
+                    if (typedName != null){
+                     var weatherData = await weather.getCityWeather(typedName);
+                     updateUI(weatherData);
+                    }
                     },
                     child: Icon(
                       Icons.location_city,
