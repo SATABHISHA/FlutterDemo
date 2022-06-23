70f38ab9c9d36934b73055f8a5da0e66078c0640
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-06-23 15:35:01 +0530

    modified

--- FlashChatFlutter/lib/screens/registration_screen.dart
+++ FlashChatFlutter/lib/screens/registration_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/components/rounded_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = 'registration_screen';
@@ -9,6 +11,9 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,8 +35,11 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
               onChanged: (value) {
                 //Do something with the user input.
+                email = value;
               },
               style: TextStyle(color: Colors.black),
               decoration: kTextFieldDecoration.copyWith(
@@ -42,8 +50,11 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 8.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
+              obscureText: true,
               onChanged: (value) {
                 //Do something with the user input.
+                password = value;
               },
               style: TextStyle(color: Colors.black),
               decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password'),
@@ -51,7 +62,20 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             SizedBox(
               height: 24.0,
             ),
-            RoundedButton(title: 'Register', colour: Colors.blueAccent, onPressed: (){},),
+            RoundedButton(
+              title: 'Register',
+              colour: Colors.blueAccent,
+              onPressed: () async {
+                try {
+                  final newUser = _auth.createUserWithEmailAndPassword(
+                      email: email, password: password);
+                  if (newUser != null){
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                }catch(e){
+                  print(e);
+                }
+            },),
           ],
         ),
       ),
