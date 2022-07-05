da35a4636893970fbfdd81c78e8d35371061e861
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-07-05 15:16:30 +0530

    modified

--- ArbOffcMgmt/lib/main.dart
+++ ArbOffcMgmt/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:arb_offc/screens/employee_list.dart';
 import 'package:arb_offc/screens/employees_home.dart';
 import 'package:arb_offc/screens/hrskill/admin_dashboard.dart';
+import 'package:arb_offc/screens/hrskill/admin_searching_screen1.dart';
 import 'package:arb_offc/screens/hrskill/admin_skill_management.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -26,7 +27,8 @@ class MyApp extends StatelessWidget {
           EmployeesHome.id : (context)=>EmployeesHome(),
           EmployeeList.id: (context)=>EmployeeList(),
           AdminDashboard.id: (context)=>AdminDashboard(),
-          AdminSkillManagement.id: (context)=> AdminSkillManagement()
+          AdminSkillManagement.id: (context)=> AdminSkillManagement(),
+          SearchSkillByAdmin.id: (context)=> SearchSkillByAdmin()
         },
       ),
     );
---
+++ ArbOffcMgmt/lib/screens/hrskill/admin_searching_screen1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SearchSkillByAdmin extends StatelessWidget {
+  // const SearchSkillByAdmin({Key? key}) : super(key: key);
+  static String id = 'SearchSkillByAdmin';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Color.fromRGBO(92, 76, 121, 1.0), title: Text('Skill Management', style: TextStyle(color: Colors.white, fontSize: 18.0),),),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
